9e857444064dfb6ccc5d268ce25f13843884995c
GMarinBorges <75048870+GMarinBorges@users.noreply.github.com>
2022-11-13 23:58:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const PORT = 1200;
 let today = new Date().toLocaleDateString()
 
-const dbUrl = "mongodb+srv://dbadmin:admin@mongo.zjgsurz.mongodb.net/test";
+const dbUrl = "mongodb+srv://admin:admin@cluster0.ow9rs4z.mongodb.net/test";
 
 //Connect to MongoDB
 mongoose.connect(dbUrl, {
@@ -85,7 +85,8 @@ app.post('/addCourse', async (req,res) => {
             courseInstructor: req.body.courseInstructor,
             courseCredits: req.body.courseCredits,
             courseID: req.body.courseID,
-            courseName: req.body.courseName
+            courseName: req.body.courseName,
+            dateEntered: new Date()
         }
 
         await Course(program).save().then(c => {
@@ -102,7 +103,8 @@ app.post('/addStudent', async (req,res) => {
         let learner = {
             fname: req.body.fname,
             lname: req.body.lname,
-            studentID: req.body.studentID
+            studentID: req.body.studentID,
+            dateEntered: new Date()
         }
 
         await Student(learner).save().then(c => {
@@ -111,6 +113,95 @@ app.post('/addStudent', async (req,res) => {
     }
     catch {
         return res.status(500).json("{message: Failed to add student - bad data}");
+    }
+});
+
+app.post('/editStudentById', async (req,res) => {
+    try {
+        let studentByID = await Student.updateOne({_id: req.body.id}, {
+            fname: req.body.fname
+        }, {upsert: true});
+
+        if (studentByID)
+            {
+                res.status(200).json("{message: Student updated}");
+            } else {
+                res.status(200).json("{message: No student updated}");
+            }
+    }
+    catch {
+        return res.status(500).json("{message: Failed to edit student - bad data}");
+    }
+});
+
+app.post('/editStudentByFname', async (req,res) => {
+    try {
+        let studentByFname = await Student.updateOne({fname: req.body.queryfname}, {
+            fname: req.body.fname,
+            lname: req.body.lname
+        }, {upsert: true});
+
+        if (studentByFname)
+            {
+                res.status(200).json("{message: Student updated}");
+            } else {
+                res.status(200).json("{message: No student updated}");
+            }
+    }
+    catch {
+        return res.status(500).json("{message: Failed to edit student - bad data}");
+    }
+});
+
+app.post('/editCourseByCourseName', async (req,res) => {
+    try {
+        let CourseByCourseName = await Course.updateOne({courseName: req.body.courseName}, {
+            courseInstructor: req.body.courseInstructor
+        }, {upsert: true});
+
+        if (CourseByCourseName)
+            {
+                res.status(200).json("{message: Course updated}");
+            } else {
+                res.status(200).json("{message: No course updated}");
+            }
+    }
+    catch {
+        return res.status(500).json("{message: Failed to edit course - bad data}");
+    }
+});
+
+app.post('/deleteCourseById', async (req,res) => {
+    try {
+        let CourseDeleteById = await Course.findOne({courseID: req.body.courseID});
+
+        if (CourseDeleteById)
+            {
+                await Course.deleteOne({courseID: req.body.courseID});
+                res.status(200).json("{message: Course deleted}");
+            } else {
+                res.status(200).json("{message: No course deleted}");
+            }
+    }
+    catch {
+        return res.status(500).json("{message: Failed to delete course - bad data}");
+    }
+});
+
+app.post('/removeStudentFromClasses', async (req,res) => {
+    try {
+        let StudentFromClasses = await Student.findOne({studentID: req.body.studentID});
+
+        if (StudentFromClasses)
+            {
+                await Student.deleteOne({studentID: req.body.studentID});
+                res.status(200).json("{message: Student deleted}");
+            } else {
+                res.status(200).json("{message: No student deleted}");
+            }
+    }
+    catch {
+        return res.status(500).json("{message: Failed to delete student - bad data}");
     }
 });
 
